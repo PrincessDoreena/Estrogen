a6ccd6b15298fd409d94740911ef49fb848d9b88
Dorina Kovacs <princessdorinakovacs@gmail.com>
2024-01-08 22:44:48 +0100

    patch

--- common/src/main/java/dev/mayaqq/estrogen/registry/common/EstrogenEffects.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/common/EstrogenEffects.java
@@ -16,7 +16,7 @@ public class EstrogenEffects {
 
     public static final Registrar<MobEffect> MOB_EFFECTS = MANAGER.get().get(Registries.MOB_EFFECT);
 
-    public static final MobEffect ESTROGEN_EFFECT = new EstrogenEffect(MobEffectCategory.BENEFICIAL, 104164161).addAttributeModifier(EstrogenAttributes.DASH_LEVEL.get(), "c2c16ccb-9acc-4f57-88e1-7b3e0c2ffe16", 1, AttributeModifier.Operation.ADDITION);
+    public static final MobEffect ESTROGEN_EFFECT = new EstrogenEffect(MobEffectCategory.BENEFICIAL, 104164161);
 
     public static void register() {
         MOB_EFFECTS.register(id("estrogen"), () -> ESTROGEN_EFFECT);
